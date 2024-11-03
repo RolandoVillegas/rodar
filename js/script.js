d63d69981f53ba7d215915afd8d6bb890fbe14a0
Rolando Villegas <rolando.villegas@gmail.com>
2024-11-03 01:34:59 -0300

    Menú principal y carrusel
    Versión 01: contiene menú principal y carrusel.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.getElementById('navbarToggle').addEventListener('click', function() {
+    const menu = document.getElementById('navbarMenu');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+});
+
+/* Función para mover el slider */
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    
+    // Ocultar la diapositiva actual
+    slides[currentSlide].classList.remove('active');
+    
+    // Actualizar el índice de la diapositiva actual
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    
+    // Mostrar la nueva diapositiva
+    const slider = document.getElementById('slider');
+    slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Mueve el slide automáticamente cada 5 segundos
+setInterval(() => moveSlide(1), 5000);
+
